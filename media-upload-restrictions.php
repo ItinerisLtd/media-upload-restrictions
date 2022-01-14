b81f5e70e93aeaba2016458ac06cf697e216cab3
codepuncher <lee.hanbury@gmail.com>
2022-01-14 11:47:41 +0000

    Initial commit

---
+++ media-upload-restrictions.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Plugin Name: Media Upload Restrictions
+ * Plugin URI: https://www.itineris.co.uk/
+ * Description: Restricts uploads to comply with our upload policy.
+ * Author: Itineris Ltd
+ * Author URI: https://www.itineris.co.uk/
+ * Version: 0.1.0
+ */
+
+/**
+ * Prevent audio files from being uploaded.
+ */
+add_filter('upload_mimes', function (array $mime_types): array {
+  return array_filter($mime_types, fn (string $mime_type): bool => false === strpos($mime_type, 'audio'));
+});
+
+/**
+ * Prevent video files from being uploaded.
+ */
+add_filter('upload_mimes', function (array $mime_types): array {
+  return array_filter($mime_types, fn (string $mime_type): bool => false === strpos($mime_type, 'video'));
+});
